64764c825313e272942953285bd0c8f08c220da6
Meraj Rizwan <meraj.rizwan9832@gmail.com>
2022-10-01 15:35:21 +0530

    logo

--- moviedataapp/src/components/header/Header.js
+++ moviedataapp/src/components/header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className="header">
             <div className="headerLeft">
-                <Link to="/"><img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" /></Link>
+                <Link to="/"><img className="header__icon" src="https://cdnimages.bigflicks.com/images/vod/movies/Carousels/igflixFull_ScreenBackground_1980x1080.png" /></Link>
                 <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
                 <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
                 <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
